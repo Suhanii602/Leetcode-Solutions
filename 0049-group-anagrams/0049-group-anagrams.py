cae72a941823665814bc25881659bde9b99c2526
Suhani Shrivastava <shrivastava.suhani06@gmail.com>
2026-08-22 21:52:29 +0530

    Time: 8 ms (99.87%), Space: 16 MB (69.93%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        result={}
+        for word in strs:
+            key = "".join(sorted(word))
+            if key in result:
+                result[key].append(word)
+            else:
+                result[key]=[word]
+        return list(result.values())
